bf50ae7338487a80f93bb42937e51824b2a48a72
Sshovon <frcshovon@gmail.com>
2021-12-02 15:08:31 +0600

    meal summary balance summary

--- src/routes/summaryRoutes.js
+++ src/routes/summaryRoutes.js
@@ -18,7 +18,7 @@ router.get('/short',auth,async(req,res)=>{
     }
 })
 
-router.get('/extended',auth,async(req,res)=>{
+router.get('/meal',auth,async(req,res)=>{
     try{
         const messID = req.mess._id;
         const [users] = await Mess.find({ _id: messID }).populate('members')
@@ -27,8 +27,33 @@ router.get('/extended',auth,async(req,res)=>{
         users.members.forEach(element => {
             const member={};
             member.name=element.name,
-            member.expense=element.expense,
             member.totalMeal=element.totalMeal
+            membersRefinedDetails=membersRefinedDetails.concat(member);
+
+        });
+        res.status(200).json({
+            totalMeal:users.totalMeal,
+            membersRefinedDetails
+        });
+        
+    }catch(e){
+        const error=e.message;
+        res.send({error})
+    }
+})
+
+router.get('/balance',auth,async(req,res)=>{
+    try{
+        const messID = req.mess._id;
+        const [users] = await Mess.find({ _id: messID }).populate('members')
+        let membersRefinedDetails=[];
+        
+        users.members.forEach(element => {
+            const member={};
+            member.name=element.name,
+            member.paid=element.expense,
+            member.charged=(element.totalMeal*users.mealCost),
+            member.balance=(member.paid-member.charged)
             membersRefinedDetails=membersRefinedDetails.concat(member);
 
         });
