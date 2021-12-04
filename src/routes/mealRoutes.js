fbfffa0f2cb049a5ea4736b91479dbc8962959e7
Sshovon <frcshovon@gmail.com>
2021-12-04 06:06:55 +0600

    frontend dev demand

--- src/routes/mealRoutes.js
+++ src/routes/mealRoutes.js
@@ -12,13 +12,34 @@ const Mess = require('../models/messModel')
 
 router.post('/show', auth, async (req, res) => {
     try {
-        const {date}=req.body
-        const dailyList=req.mess.mealList.filter(element=>element.date===date);
+
+        const { date } = req.body
+        let [dailyList] = req.mess.mealList.filter(element => element.date === date);
+        if (dailyList == undefined) {
+            let demand = {
+                date
+            }
+            demand.dailyList = [];
+            const allUser = await Mess.find({ _id: req.mess._id }).populate('members')
+            const members = allUser[0].members.filter(() => true);
+            members.forEach(member => {
+                demand.dailyList.push({
+                    name:member.name,
+                    breakfast:0,
+                    lunch:0,
+                    dinner:0,
+                    ownerID:member._id
+                })
+                
+            })
+        
+        dailyList=demand
+        }
         res.send(dailyList)
 
     } catch (e) {
-        const error=e.message
-        res.send({error})
+        const error = e.message
+        res.send({ error })
     }
 })
 
@@ -37,40 +58,40 @@ router.post('/add', [auth, roleChecker], async (req, res) => {
                 }
             }
         });
-        const totalMeal=await User.doCount(dailyList);
+        const totalMeal = await User.doCount(dailyList);
         req.mess.totalMeal += totalMeal
         await req.mess.generateMealCost();
-        res.send({result:"success"})
+        res.send({ result: "success" })
 
     } catch (e) {
-        const error=e.message
-        res.send({error})
+        const error = e.message
+        res.send({ error })
     }
 })
 
 
 
-router.patch('/update',[auth,roleChecker],async (req,res)=>{
-    try{
+router.patch('/update', [auth, roleChecker], async (req, res) => {
+    try {
         const { date, dailyList } = req.body;
         const exists = await Mess.find({ "mealList.date": date });
         if (!exists.length)
             throw new Error("Date doesn't exists");
 
-        const prevDailyList=exists[0].mealList.find(element => element.date===date)
+        const prevDailyList = exists[0].mealList.find(element => element.date === date)
         const prevTotalMeal = await User.updateDoCount(prevDailyList.dailyList);
-        const newTotalMeal=await User.doCount(dailyList);
-        await req.mess.updateMealList(date,dailyList,newTotalMeal-prevTotalMeal);
+        const newTotalMeal = await User.doCount(dailyList);
+        await req.mess.updateMealList(date, dailyList, newTotalMeal - prevTotalMeal);
         await req.mess.generateMealCost();
-        res.send({result:"success"})
-        
-    }catch(e){
-        const error=e.message
-        res.send({error});
+        res.send({ result: "success" })
+
+    } catch (e) {
+        const error = e.message
+        res.send({ error });
     }
 })
 
 
 
 
-module.exports= router;
+module.exports = router;
