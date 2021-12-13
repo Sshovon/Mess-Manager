c34a63bae8bd228f3a5d7406f3008a6ad444b7d7
Sshovon <frcshovon@gmail.com>
2021-12-13 06:53:41 +0600

    mongdb url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/mess-manager',{ 
+mongoose.connect(process.env.DB_URL,{ 
     useNewUrlParser: true,
     useUnifiedTopology:true
 })
